9b2310a7e21870c568a2286c3038b7498bcb92e1
root <root@DESKTOP-SMDET76>
2023-11-19 15:45:44 +0800

    remove sensitive file db_config.py db_config_local.py

--- src/config/db_config.py
+++
@@ -1,8 +0,0 @@
-DB_CONFIG = {
-    # 'DBHOST' : '103.40.3.37',
-    'DBHOST' : '45.77.98.126',
-    'DBUSER' : 'scraper',
-    'DBPASS' : 'fhetI99v@rQPCQpRp',
-    'DBNAME' : 'scraped_data'
-    
-}
--- src/config/db_config_local.py
+++
@@ -1,7 +0,0 @@
-DB_CONFIG = {
-    'DBHOST' : 'localhost',
-    'DBUSER' : 'testuser',
-    'DBPASS' : 'password',
-    'DBNAME' : 'scraped_data'
-    
-}
