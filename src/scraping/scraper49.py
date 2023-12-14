eda43429ced66497eec0ab5809881436ea99cfd8
Terence Albert Galos <terence.galos@gmail.com>
2023-12-14 20:36:42 +0800

    scraper49 on progress

--- src/scraping/scraper49.py
+++ src/scraping/scraper49.py
@@ -1,4 +1,4 @@
-import requests,json
+import requests,time, json
 from bs4 import BeautifulSoup
 import concurrent.futures
 from fake_useragent import UserAgent
@@ -20,33 +20,28 @@ class Scraper49:
     
     
     
-    def scrape_single(self,url,data):
-        
+    def scrape_single(self,url):
+
         headers = {
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
             'Accept-Encoding': 'gzip, deflate, br',
             'Accept-Language': 'en-US,en;q=0.5',
             'Connection': 'keep-alive',
-            'Content-Length': '17',
-            'Content-Type': 'application/x-www-form-urlencoded',
+            'DNT': '1',
             'Host': 'arc-sos.state.al.us',
-            'Origin': 'https://arc-sos.state.al.us',
-            'Referer': 'https://arc-sos.state.al.us/cgi/uccname.mbr/input',
             'Sec-Fetch-Dest': 'document',
             'Sec-Fetch-Mode': 'navigate',
-            'Sec-Fetch-Site': 'same-origin',
+            'Sec-Fetch-Site': 'none',
             'Sec-Fetch-User': '?1',
+            'Sec-GPC': '1',
             'Upgrade-Insecure-Requests': '1',
-            'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:120.0) Gecko/20100101 Firefox/120.0'
+            'User-Agent': self.ua.random
         }
 
 
-        response = requests.get("https://arc-sos.state.al.us/cgi/uccname.mbr/input")
-        cookies = response.cookies
-        response = requests.post(self.url,headers=headers, cookies=cookies, data=data)
-        print(f'{url}')
 
-        # print(response.text)
+        print(f'Extracting info. URL: {url}')
+        response = requests.get(url,headers=headers)
         print(f'Status code: {response.status_code}')
 
 
@@ -58,13 +53,34 @@ class Scraper49:
         # Parse the HTML content with BeautifulSoup
 
         soup = BeautifulSoup(response.content,'html.parser')
-        
-        urls = []
-        tr_elements = soup.find_all("tr")
-        for tr in tr_elements:
-            url = f'{self.baseurl}{tr.find('a')['href']}'
-            print(f'Url: {url}')
-            urls.append(url)
+
+        result_dict = {'debtor_name':'','debtor_address':'','secured_party_name':'','secured_party_address':''}
+
+        # Extract debtor info
+        debtor_info = soup.find_all('td',class_='aiSosDetailValue')[7:8]
+
+        # for d in debtor_info:
+        #     print(f'info: {d.get_text(separator='\n').split('\n')}')
+
+        if debtor_info:
+            debtor = debtor_info[0].get_text(separator='\n')
+            debtor_name = debtor.split("\n")[0]
+            debtor_address = ' '.join(debtor.split("\n")[1:])
+            result_dict.update({'debtor_name':debtor_name})
+            result_dict.update({'debtor_address':debtor_address})
+
+        # Extract secured party info
+        secured_party_info = soup.find_all('td',class_='aiSosDetailValue')[8:9]
+        secured_party = secured_party_info[0].get_text(separator='\n')
+        if secured_party_info:
+            secured_party_name = secured_party.split("\n")[0]
+            secured_party_address = ' '.join(secured_party.split("\n")[1:])
+            result_dict.update({'secured_party_name':secured_party_name})
+            result_dict.update({'secured_party_address':secured_party_address})
+
+        print(result_dict)
+
+        return result_dict
 
         
         
@@ -81,42 +97,73 @@ class Scraper49:
     
     
     
-    def scrape_pages(self,last_interrupt_char):
+    def scrape_pages(self, data, batch_size, last_interrupt_char=None):
 
-        def get_nextpage_url(soup):
-            next_button = soup.find(text='Next >>')
+        # Get next page
+        def get_next_page_url(soup):
+            # print(soup.contents)
+            next_button = soup.find('a',text=lambda text : text and '>>' in text.lower())
             if next_button:
-                next_url = next_button.find_parent('a')['href']
-                return self.baseurl+next_url
-            
+                next_url = self.url + next_button.get('href')
+                print(f'Next url : {next_url}')
+                return next_url
+            print("Next button n/a. Pagination done? ")
             return None
+        
+        def get_page_links(soup):
+            tr_elements = soup.find_all("tr")
+            page_results = [f'{self.baseurl}{tr.find('a')['href']}' for tr in tr_elements[1:-1]]
+            return page_results
+                
 
 
+        # 1 letter search; Loop all uppercase
         for char in ascii_uppercase[last_interrupt_char:]:
+            print(f"Extract search results for '{char}'")
             data = {"search":f"{char}","type":"ALL"}
-
-            response = requests.get("https://arc-sos.state.al.us/cgi/uccname.mbr/input")
-            cookies = response.cookies
-            response = requests.post(self.url, cookies=cookies, data=data)
-            print(f'{url}')
-
-            # print(response.text)
+            
+            # get page using post request
+            response = requests.post(self.url, data=data)
+            print(f'Scraping entries in url: {self.url}')
             print(f'Status code: {response.status_code}')
 
-            # raise for failed requests
-            # response.raise_for_status()
-
+            # Get page results
             # Parse the HTML content with BeautifulSoup
             soup = BeautifulSoup(response.content,'html.parser')
             
-            urls = []
-            tr_elements = soup.find_all("tr")
-            for tr in tr_elements:
-                url = f'{self.baseurl}{tr.find('a')['href']}'
-                print(f'Url: {url}')
-                urls.append(url)
-            
-            yield urls
+            urls = [] # Page results here
+
+            # Get first page results and store
+            url = get_page_links(soup)
+            urls.extend(url)
+            # print("\n".join(url))
+
+            # print(f'Looping urls and extract their info.')
+            # for url in urls:
+            #     self.scrape_single(url)
+
+            # Loop pagination
+            while True:
+                try:
+                    next_url = get_next_page_url(soup)
+                    print(f'next page : {next_url}')
+                    response = requests.get(next_url)
+                    soup = BeautifulSoup(response.text,'html.parser')
+                    # page_urls = get_page_links(soup)
+                    # urls.extend(page_urls)
+                    # print(urls)
+                    # for url in urls:
+                    #     self.scrape_single(url)
+                    # urls = [] # reset urls after processing
+                except Exception as e:
+                    print(f"Error getting page. Pagination done?\nError: {e}")
+                    break
+
+            # with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
+            #     for i in range(len(urls),batch_size):
+            #         batch_results = [results for results in list(executor.map(scrape_single_thread,urls[i,i+batch_size]))]
+            #         yield batch_results
+
 
 
 
@@ -146,4 +193,6 @@ class Scraper49:
         # for char in ascii_uppercase:
         data = {"search":"a","type":"ALL"}
 
-        self.scrape_single(self.url,data)
+        self.scrape_pages(data,batch_size)
+
+        # self.scrape_single(self.url,data)
