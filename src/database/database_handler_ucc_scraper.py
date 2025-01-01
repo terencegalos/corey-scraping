d1b858dd3f2d41158d66913f5d5de25618f7d9d1
Terence Albert Galos <terence.galos@gmail.com>
2025-01-01 22:15:26 +0800

    new batch processing feature; reduce database calls by processing in batch

--- src/database/database_handler_ucc_scraper.py
+++ src/database/database_handler_ucc_scraper.py
@@ -1,5 +1,6 @@
 import mysql.connector
 
+
 class DatabaseHandler:
     def __init__(self,host,user,password,database,table_names):
         # Connection to the database
@@ -28,10 +29,14 @@ class DatabaseHandler:
         # Commit
         self.conn.commit()
         
+        
+        
+        
     def store_data(self, table_name, data_list):
         for data in data_list:
             print(data)
-    # Insert data into the database with explicit default values
+        
+        # Insert data into the database with explicit default values
         for data in data_list:
             self.cursor.execute(f'''
                                 INSERT INTO {table_name} (debtor_name, debtor_address, secured_party_name, secured_party_address, datetime_created)
@@ -40,10 +45,45 @@ class DatabaseHandler:
 
         # Commit changes
         self.conn.commit()
+        
+        
+        
+        
+    def store_data_batch(self,table_name,data_list):
+        if not data_list:
+            return
+        
+        print(f"Preparing to store {len(data_list)} records in {table_name}")
+        
+        # Prepare the SQL string for bulk insert
+        placeholders = ','.join(['(%s,%s,%s,%s,DEFAULT)' for _ in data_list])
+        
+        sql = f"""
+        INSERT INTO {table_name} (debtor_name,debtor_address,secured_party_name,secured_party_address,date_time_created)
+        VALUES {placeholders}
+        """
+        
+        # Flatten the list of dicts to a list of tuples for the query
+        flat_data = [(d['debtor_name'],d['debtor_address'],d['secured_party_name'],d['secured_party_address']) for d in data_list]
+        
+        try:
+            # Execute bulk insert
+            self.cursor.executemany(sql,flat_data)
+            
+            # Commit changes - commit in batches if data_list is very large
+            self.conn.commit()
+            print(f"Successfully inserted {len(data_list)} records into {table_name}")
+        except mysql.connector.Error as err:
+            print(f"Failed to insert data into {table_name}: {err}")
+            self.conn.rollback() # Rollback in case of error
+        
+        
+        
+        
 
     def data_exists(self,table_name,data):
         # Create a SQL query that checks if a record exists
-        query = f'SELECT * FROM {table_name} WHERE debtor_name = %s AND debtor_address = %s AND secured_party_name = %s AND secured_party_address = %s'
+        query = f'SELECT 1 FROM {table_name} WHERE debtor_name = %s AND debtor_address = %s AND secured_party_name = %s AND secured_party_address = %s limit 1'
 
         # Execute the query
         self.cursor.execute(query,(data['debtor_name'],data['debtor_address'],data['secured_party_name'],data['secured_party_address']))
@@ -53,7 +93,36 @@ class DatabaseHandler:
 
         # If a record was found, return True. Otherwise, return False.
         return result is not None
+    
+    
+    
+    def batch_data_exists(self, table_name,data_list):
+        """usage: new_results = db_handler.batch_data_exists(scraper.table_name,batch_results)"""
+        if not data_list:
+            return 
+        
+        # Prepare placeholders for SQL IN clause
+        placeholders = ','.join(['(%s,%s,%s,%s)' for _ in data_list])
+        
+        query = f'''
+        SELECT debtor_name, debtor_address,secured_party_name, secured_party_address
+        FROM {table_name}
+        WHERE (debtor_name, debtor_address, secured_party_name, secured_party_address) IN ({placeholders})
+        '''
+         
+        # Flatten the list of dicts into a list of tuples for the query
+        flat_data = [(d['debtor_name'],d['debtor_address'],d['secured_party_name'],d['secured_party_address']) for d in data_list]
+        
+        self.cursor.execute(query,[item for sublist in flat_data for item in sublist])
+        existing = self.cursor.fetchall()
+        
+        # Convert query results to a set for fast lookup
+        existing_set = set(map(tuple, existing))
+        
+        # Check which items exist
+        return [data for data in data_list if tuple([data[field] for field in ['debtor_name','debtor_address','secured_party_name','secured_party_address']]) not in existing_set]
 
+        
         
     def close_connection(self):
         # Close the db connection
--- src/main_ucc_scraper.py
+++ src/main_ucc_scraper.py
@@ -2,8 +2,9 @@ import sys,importlib
 import schedule
 import time
 import asyncio
-
 import telegram_notifier.telegram_notifier as notifier
+
+
 
 num_scraper = sys.argv[1]
 
@@ -11,9 +12,12 @@ module_name = f'scraping.scraper{num_scraper}'
 Scraper = getattr(importlib.import_module(module_name),f'Scraper{num_scraper}')
 
 from database.database_handler_ucc_scraper import DatabaseHandler
-from config.db_config import DB_CONFIG
+from config.db_config_local import DB_CONFIG
 from log.logger_config import configure_logger
 import logging
+
+
+
 
 
 def main():
@@ -52,30 +56,38 @@ def main():
         for batch_results in scraper.scrape_with_refcodes(last_interrupt_char=last_char,starting_page=last_page):
             # Store data in the db
             logger.info(batch_results)
-            logger.info(f"Storing batch to database...")
+            
             # db_handlermysqld.store_data(scraper.table_name,batch_results)
-            db_handler.store_data(scraper.table_name,[result for result in batch_results if not db_handler.data_exists(scraper.table_name,result)]) # adding db check if result is already in db
+            new_results = db_handler.batch_data_exists(scraper.table_name,batch_results)
+            if new_results:
+                logger.info(f"Storing to results {new_results} to {scraper.table_name}.")
+                # db_handler.store_data(scraper.table_name,[result for result in batch_results if not db_handler.data_exists(scraper.table_name,result)]) # adding db check if result is already in db
+                db_handler.store_data_batch(scraper.table_name,new_results)
+            else:
+                logger.info('Nothing new to save.')
         
         # Reset last interrupt txt file once done
         with open(scraper.last_interrupt_txt,'w') as f:
             f.write('')
 
         logger.info("Scraping and storing data completed successfully.")
+        
     except Exception as e:
         logger.error(f"An error occurred: {str(e)}",exc_info=True)
         message = f"⚠️ **Notice:** Scraper {scraper_name} has been interrupted. Check error log for details."
-        asyncio.run(notifier.send_telegram_message(message))
+        # asyncio.run(notifier.send_telegram_message(message))
+        
     finally:
         # Close the db connection
         db_handler.close_connection()
 
 
 
-schedule.every().day.at("13:23").do(main)
+# schedule.every().day.at("10:30").do(main)
 
 
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+# while True:
+#     schedule.run_pending()
+#     time.sleep(1)
 
-# main()
+main()
